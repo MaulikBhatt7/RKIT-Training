e57481cd6d642221c75ab6561fc0bf8f30a295f8
MaulikBhatt7 <maulikbhatt890@gmail.com>
2025-01-08 10:13:49 +0530

    ORM update

--- Advance API/ORMDemo/Models/DTO/DTOSTD01.cs
+++ Advance API/ORMDemo/Models/DTO/DTOSTD01.cs
@@ -11,38 +11,38 @@ namespace ORMDemo.Models.DTO
         /// <summary>
         /// Gets or sets the student ID.
         /// </summary>
-        [JsonProperty("P01101")]
+        [JsonProperty("D01101")]
         public int D01F01 { get; set; }
 
         /// <summary>
         /// Gets or sets the student name.
         /// </summary>
-        [JsonProperty("P01102")]
+        [JsonProperty("D01102")]
         public string D01F02 { get; set; }
 
         /// <summary>
         /// Gets or sets the student age.
         /// </summary>
-        [JsonProperty("P01103")]
+        [JsonProperty("D01103")]
         public int D01F03 { get; set; }
 
         /// <summary>
         /// Gets or sets the city ID associated with the student.
         /// This is an optional property (nullable).
         /// </summary>
-        [JsonProperty("P01104")]
+        [JsonProperty("D01104")]
         public int? D01F04 { get; set; }
 
         /// <summary>
         /// Gets or sets the student's marks.
         /// </summary>
-        [JsonProperty("P01105")]
+        [JsonProperty("D01105")]
         public int D01F05 { get; set; }
 
         /// <summary>
         /// Gets or sets the student's grade.
         /// </summary>
-        [JsonProperty("P01106")]
+        [JsonProperty("D01106")]
         public char D01F06 { get; set; }
     }
 }
