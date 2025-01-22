685890b4025efd5c8b3ca3ee2c9cf273b4129948
MaulikBhatt7 <maulikbhatt890@gmail.com>
2025-01-22 18:51:57 +0530

    .Net Core Fundamental Demo added

---
+++ DotNet Core/CoreFundamentalDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoreFundamentalDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
