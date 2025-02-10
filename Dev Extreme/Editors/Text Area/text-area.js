bdace9898d9712055142a62ecb0ddf11134728fb
MaulikBhatt7 <maulikbhatt890@gmail.com>
2025-02-10 15:31:32 +0530

    Text Area code added

---
+++ Dev Extreme/Editors/Text Area/text-area.js
@@ -0,0 +1,29 @@
+﻿$(() => {
+    $("#textArea").dxTextArea({
+        // Enables or disables the auto-resize feature
+        autoResizeEnabled: true,
+
+        // Sets the minimum height of the text area
+        minHeight: 20,
+
+        // Sets the maximum height of the text area
+        maxHeight: 100,
+
+        // Specifies the placeholder text for the text area
+        placeholder: "U.S.A",
+
+        // Sets the maximum number of characters allowed in the text area
+        maxLength: "25",
+
+        // Disables the spellcheck feature
+        spellcheck: false, // default value is true.
+
+        // Sets the initial value of the text area
+        value: "Hello",
+
+        // Event handler for the value changed event
+        onValueChanged: (e) => {
+            console.log("Value Changed: " + e.value);
+        }
+    });
+});
